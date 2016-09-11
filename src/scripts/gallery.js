41d119a0f1499cc31167a4a2a115c1a777dbc1f2
ingaSheremet <inga@tiestegroup.com>
2016-09-11 22:20:39 +0300

    add jq

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,7 +1,12 @@
 /**
  * Created by In on 11.09.2016.
  */
+$(document).ready(function(){
+    $(button).on('click', function(){
+        sayHi()
+    })
+})
 function sayHi(){
     alert('Hello World!')
 }
-sayHi();
+
