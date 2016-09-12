6da5063c530b798da917751d51860c4387c97095
ingaSheremet <inga@tiestegroup.com>
2016-09-13 01:08:47 +0300

    add accordion

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,3 +1,22 @@
 /**
  * Created by In on 11.09.2016.
  */
+$(function($) {
+    var allAccordions = $('.accordion-data');
+    var allAccordionItems = $('.accordion-item');
+    $('.accordion > .accordion-item').click(function() {
+        if($(this).hasClass('open'))
+        {
+            $(this).removeClass('open');
+            $(this).next().slideUp("slow");
+        }
+        else
+        {
+            allAccordions.slideUp("slow");
+            allAccordionItems.removeClass('open');
+            $(this).addClass('open');
+            $(this).next().slideDown("slow");
+            return false;
+        }
+    });
+});
