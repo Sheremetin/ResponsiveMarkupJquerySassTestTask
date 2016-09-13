d61ea9f02f82e4c1eec82f0a5168be9861c97277
ingaSheremet <inga@tiestegroup.com>
2016-09-13 17:37:19 +0300

    template

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -4,17 +4,20 @@
 $(function($) {
     var allAccordions = $('.accordion-data');
     var allAccordionItems = $('.accordion-item');
-    $('.accordion > .accordion-item').click(function() {
+    $('.accordion-item-wrp > .accordion-item').click(function() {
         if($(this).hasClass('open'))
         {
             $(this).removeClass('open');
+            $(this).parent().removeClass('open');
             $(this).next().slideUp("slow");
         }
         else
         {
             allAccordions.slideUp("slow");
             allAccordionItems.removeClass('open');
+            allAccordionItems.parent().removeClass('open');
             $(this).addClass('open');
+            console.log($(this).parent().addClass('open'));
             $(this).next().slideDown("slow");
             return false;
         }
